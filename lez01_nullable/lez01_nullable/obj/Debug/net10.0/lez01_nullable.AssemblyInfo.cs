d0a8950fa16bf7158f1ad27582bd8df60fa47506
Giovanni Pace <g.pace@braintechinnovations.it>
2026-09-04 15:04:19 +0200

    Aggiunta gestione collectio custom

--- lez01_nullable/lez01_nullable/obj/Debug/net10.0/lez01_nullable.AssemblyInfo.cs
+++ lez01_nullable/lez01_nullable/obj/Debug/net10.0/lez01_nullable.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lez01_nullable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01e07a084ae9a09bf859ca180f1053bc001f4911")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01d18119374da27276eb0792e7d2e01d5fa2cdc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("lez01_nullable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lez01_nullable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ lez01_nullable/lez08_code/Program.cs
@@ -0,0 +1,80 @@
+﻿#region Semplice combo Queue HashSet
+//Queue<string> waiting = new();
+//HashSet<string> completed = new();
+
+//Add("Task 1");
+//Add("Task 1");
+//Add("Task 3");
+
+//while(waiting.TryDequeue(out string? ticket))
+//{
+//    Console.WriteLine($"Processing {ticket}...");
+//}
+
+//void Add(string ticket)
+//{
+//    if (!completed.Add(ticket))
+//    {
+//        Console.WriteLine($"Ticket {ticket} already inserted.");
+//        return;
+//    }
+
+//    waiting.Enqueue(ticket);
+//    Console.WriteLine($"Ticket {ticket} added to the waiting queue.");
+//}
+#endregion
+
+Catalog<Product> catalog = new();
+
+try
+{
+    catalog.Add(new Product("P001", "Mouse", 12.99m));
+    catalog.Add(new Product("P001", "Laptop", 999.99m));
+}
+catch (InvalidOperationException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+if(catalog.TryGet("P001", out Product? product))
+{
+    Console.WriteLine($"Product found: {product!.Name} - ${product.Price}");
+}
+else
+{
+    Console.WriteLine("Product not found.");
+}
+
+Console.WriteLine(catalog.Count);
+
+public sealed record Product(string Id, string Name, decimal Price) : IHasId;
+
+public interface IHasId
+{
+    string Id { get; }
+}
+
+public sealed class Catalog<T> where T : IHasId
+{
+    private readonly Dictionary<string, T> _items = new(StringComparer.OrdinalIgnoreCase);
+    
+    public int Count => _items.Count;
+
+    public void Add(T item)
+    {
+        if (!_items.TryAdd(item.Id, item))
+        {
+            throw new InvalidOperationException($"Item with ID '{item.Id}' already exists.");
+        }
+    }
+
+    public bool TryGet(string id, out T? item)
+    {
+        return _items.TryGetValue(id, out item);
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        return _items.Values;
+    }
+}   
